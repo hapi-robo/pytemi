4bddfad303de2eee66894384e34ab0ee5c9fd9bb
Yulia-Pryima <yulia.pryima@gmail.com>
2020-09-02 16:23:43 +0900

    Add Changes to Go to Random location script

---
+++ scripts/go_to_random_location.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Temi randomly and continuously goes to any saved location
+
+"""
+import pytemi as temi
+import time
+import random
+
+
+# parameters
+MQTT_HOST = "34.83.131.39"
+MQTT_PORT = 1883
+MQTT_USERNAME = "connect"
+MQTT_PASSWORD = "hrstqa123"
+TEMI_SERIAL = "00119260058"
+
+# connect to the MQTT server
+mqtt_client = temi.connect(MQTT_HOST, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD)
+
+# create robot object
+robot = temi.Robot(mqtt_client, TEMI_SERIAL)
+
+# --------------------------------------------------------------
+
+
+# Go to random location
+
+LOCATIONS = ["Kitchen", "Home Base", "Entrance", "Living room", "Kitchen"]
+
+while True:
+	robot.goto(random.choice(LOCATIONS)) # command the robot to go to a random saved location
+	time.sleep(10) # wait some time for action to complete
+
+
+
